9c365d489488ab778b2a5727907dd8cc4cbc84e1
wabemusic <jvwatanabe@gmail.com>
2026-05-26 21:15:55 -0300

    projeto finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Home from "./pages/Home/Home";
 import Inclusion from "./pages/Inclusion/Inclusion";
-import Search from "./pages/Search/Search";
 import MySongs from "./pages/MySongs/MySongs";
 
 function App() {
